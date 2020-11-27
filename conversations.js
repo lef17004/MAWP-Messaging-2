af02d184aca83234b471a2dfb66166023f189319
Glitch (hello-webpage) <none>
2020-11-27 20:22:33 +0000

    👾🌗 Checkpoint ./segue.js:24681360/64 ./login.js:24681360/40 ./index.html:24681360/67 ./conversations.js:24681360/60

--- conversations.js
+++ conversations.js
@@ -5,6 +5,7 @@ function startConversation() {
 function loadConversations(id) {
    backendLoadConversations(id, function(result) {
       let conversation = document.createElement("Button")
+      conversation.setAttribute("onclick", "signOut()")
       conversation.innerHTML = result.email
       document.querySelector("#list").appendChild(conversation)
       document.querySelector("#list").appendChild(document.createElement("br"))
--- segue.js
+++ segue.js
@@ -1,11 +1,11 @@
-function segueToConversations() {
-  const id = "XZDVCzIoQUg9OC7uQFowjflINAV2"
+function segueToConversations(id) {
+  
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("body").innerHTML =
       this.responseText;
-      
+      loadConversations(id)
     }
   };
   xhttp.open("GET", "conversations.html", true);
