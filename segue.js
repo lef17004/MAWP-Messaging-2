41ff58634cb5479f39504f48d1cce950f2568f61
Glitch (hello-webpage) <none>
2020-11-27 17:59:05 +0000

    👣🎷 Checkpoint ./segue.js:24681360/858 ./conversations.js:24681360/44

--- segue.js
+++ segue.js
@@ -1,14 +1,21 @@
 function loadConversations() {
+  const id = "XZDVCzIoQUg9OC7uQFowjflINAV2"
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("body").innerHTML =
       this.responseText;
       
+      backendLoadConversations(id, function(result) {
+        let conversation = document.createElement("Button")
+        conversation.innerHTML = result.email
+        document.querySelector("#list").appendChild(conversation)
+        document.querySelector("#list").appendChild(document.createElement("br"))
+        
+      })
       
-      let p = document.createElement("BUTTON")
-      p.innerHTML = "I am a P!"
-      document.querySelector("#list").appendChild(p)
+      
+      
       
       
     }
