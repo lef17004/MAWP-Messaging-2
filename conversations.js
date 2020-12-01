6557d99a5d2b5ab93bf545d456b8ebe5de615e27
Glitch (hello-webpage) <none>
2020-12-01 23:28:41 +0000

    🎬🎤 Checkpoint ./conversations.html:24681360/23 ./conversations.js:24681360/2502

--- conversations.js
+++ conversations.js
@@ -14,5 +14,49 @@ function loadConversations(id) {
       conversation.innerHTML = result.email
       document.querySelector("#list").appendChild(conversation)
       document.querySelector("#list").appendChild(document.createElement("br"))
+     
+     let conv = document.createElement("DIV")
+     conv.className = "d-flex bd-highlight"
+     
+     let lev2 = document.createElement("DIV")
+     lev2.className = "img_cont"
+     
+     let image = document.createElement("IMG")
+     image.src = "https://i.pinimg.com/564x/d9/56/9b/d9569bbed4393e2ceb1af7ba64fdf86a.jpg"
+     image.className = "rounded-circle user_img"
+     
+     let span1 = document.CreateElement("SPAN")
+     span1.className = "online_icon"
+     
+     let userInfo = document.CreateElement("DIV")
+     userInfo.className = "user_info"
+     
+     let span2 = document.createElement("SPAN")
+     span2.innerHTML = "Michael"
+     
+     let p = document.createElement("P")
+     p.innerHTML = "Michael is online"
+     
+     
+     userInfo.appendChild(span2)
+     userInfo.appendChild(p)
+     
+     
+     
     })
 }
+
+
+// <div class="d-flex bd-highlight">
+//           <div class="img_cont">
+//             <img
+//               src="https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
+//               class="rounded-circle user_img"
+//             />
+//             <span class="online_icon"></span>
+//           </div>
+//           <div class="user_info">
+//             <span>Ya Boi</span>
+//             <p>Ya Boi is online</p>
+//           </div>
+//         </div>
